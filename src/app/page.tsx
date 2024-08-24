22f583356691f1edb30d98907a3e5b0ab7494a18
sainiram <sainiram.tnt@gmail.com>
2024-08-24 18:10:31 +0530

    add new section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,6 +25,175 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <section className="py-20 bg-[#f6f6f6]">
+        <div className="container">
+          <h5 className=" text-center font-bold text-[1.125rem] text-[var(--theme)]">
+            WHAT WE DO
+          </h5>
+          <h4 className="text-[2.5rem] font-light text-center text-black">
+            The Car and Limousine Service
+          </h4>
+
+          <div className="flex flex-wrap items-center justify-start pt-20">
+            <div className="lg:w-4/12 md:w-6/12 w-full p-5">
+              <div className="bg-white">
+                <div className="relative z-0">
+                  <img
+                    src="https://quanticalabs.com/wp_themes4/autoride/wp-content/uploads/sites/2/2019/10/image_06.jpg"
+                    className="w-full z-0 "
+                    alt=""
+                  />
+                  <div className="absolute z-10 top-1/2 left-1/2 -translate-y-3/4 -translate-x-1/2 w-36 h-20 border-2 border-b-0 border-white rounded-tl-full rounded-tr-full"></div>
+                  <i className="ph-light ph-buildings absolute z-10 top-1/2 left-1/2  -translate-x-1/2  text-white text-6xl"></i>
+                  <div className="absolute w-full h-full bg-[rgba(133,140,146,0.4)] z-[1px] top-0 left-0"></div>
+                </div>
+                <div className="p-10 text-center">
+                  <a
+                    href=""
+                    className="block text-xl text-[var(--theme)] font-bold mb-5 "
+                  >
+                    CORPORATE TRAVEL
+                  </a>
+                  <p className="text-black font-light text-[1.125rem]">
+                    We provide business services and our own vehicle fleet so
+                    that you can reach your destination safely, in comfort and
+                    always on time
+                  </p>
+                </div>
+              </div>
+            </div>
+            {/* <!-- col-end --> */}
+            <div className="lg:w-4/12 md:w-6/12 w-full p-5">
+              <div className="bg-white">
+                <div className="relative z-0">
+                  <img
+                    src="https://quanticalabs.com/wp_themes4/autoride/wp-content/uploads/sites/2/2019/10/image_07.jpg"
+                    className="w-full z-0 "
+                    alt=""
+                  />
+                  <div className="absolute z-10 top-1/2 left-1/2 -translate-y-3/4 -translate-x-1/2 w-36 h-20 border-2 border-b-0 border-white rounded-tl-full rounded-tr-full"></div>
+                  <i className="ph-light ph-airplane-tilt absolute z-10 top-1/2 left-1/2  -translate-x-1/2  text-white text-6xl"></i>
+                  <div className="absolute w-full h-full bg-[rgba(133,140,146,0.4)] z-[1px] top-0 left-0"></div>
+                </div>
+                <div className="p-10 text-center">
+                  <a
+                    href=""
+                    className="block text-xl text-[var(--theme)] font-bold mb-5 "
+                  >
+                    AIRPORT TRANSFER
+                  </a>
+                  <p className="text-[#778591] font-light text-[1.125rem]">
+                    We provide airport transfers to and from all the major
+                    airports. Our service is the perfect option for when you are
+                    travelling for business or pleasure
+                  </p>
+                </div>
+              </div>
+            </div>
+            {/* <!-- col-end --> */}
+            <div className="lg:w-4/12 md:w-6/12 w-full p-5">
+              <div className="bg-white">
+                <div className="relative z-0">
+                  <img
+                    src="https://quanticalabs.com/wp_themes4/autoride/wp-content/uploads/sites/2/2019/04/image_08.jpg"
+                    className="w-full z-0 "
+                    alt=""
+                  />
+                  <div className="absolute z-10 top-1/2 left-1/2 -translate-y-3/4 -translate-x-1/2 w-36 h-20 border-2 border-b-0 border-white rounded-tl-full rounded-tr-full"></div>
+                  <i className="ph-light ph-sliders-horizontal absolute z-10 top-1/2 left-1/2  -translate-x-1/2  text-white text-6xl"></i>
+                  <div className="absolute w-full h-full bg-[rgba(133,140,146,0.4)] z-[1px] top-0 left-0"></div>
+                </div>
+                <div className="p-10 text-center">
+                  <a
+                    href=""
+                    className="block text-xl text-[var(--theme)] font-bold mb-5 "
+                  >
+                    EVENT SERVICE
+                  </a>
+                  <p className="text-[#778591] font-light text-[1.125rem]">
+                    Whether it is a short shopping trip or a long-distance
+                    journey, we will get you there in comfort plus we will
+                    handle the entire event efficiently
+                  </p>
+                </div>
+              </div>
+            </div>
+            {/* <!-- col-end --> */}
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="flex flex-wrap items-center">
+          <img
+            src="https://quanticalabs.com/wp_themes4/autoride/wp-content/uploads/sites/2/2019/09/image_01-1.jpg?id=10425"
+            className="w-6/12"
+            alt=""
+          />
+          <div className="w-6/12 p-12">
+            <h5 className=" text-center font-bold text-[1.125rem] text-[var(--theme)]">
+              WHY CHOOSE US
+            </h5>
+            <h4 className="text-[2.5rem] font-light text-center text-black mb-14">
+              Service Tailored to You
+            </h4>
+            <p className="text-[#778591] font-light text-[1.375rem] text-center mb-14">
+              When organizing a special event you and your family or friends
+              might want to have a good time and not worry about driving. Our
+              experienced chauffeurs will guide you through what every city you
+              are in.
+            </p>
+
+            <div className="flex flex-wrap justify-around items-center">
+              <div className="w-3/12">
+                <div className="w-12 h-12 rounded-full  bg-[#eaecee] relative mx-auto block">
+                  <i className="ph-light ph-shield-chevron text-6xl text-[var(--theme)] absolute bottom-1 right-1"></i>
+                </div>
+                <h6 className="font-bold text-[1.125rem] text-[#2C3E50] mt-8 text-center">
+                  Safety First
+                </h6>
+              </div>
+              <div className="w-3/12">
+                <div className="w-12 h-12 rounded-full  bg-[#eaecee] relative mx-auto block">
+                  <i className="ph-light ph-bus text-6xl text-[var(--theme)] absolute bottom-1 right-1"></i>
+                </div>
+                <h6 className="font-bold text-[1.125rem] text-[#2C3E50] mt-8 text-center">
+                  Coach Buses
+                </h6>
+              </div>
+              <div className="w-3/12">
+                <div className="w-12 h-12 rounded-full  bg-[#eaecee] relative mx-auto block">
+                  <i className="ph-light ph-lighthouse text-6xl text-[var(--theme)] absolute bottom-1 right-1"></i>
+                </div>
+                <h6 className="font-bold text-[1.125rem] text-[#2C3E50] mt-8 text-center">
+                  Safe Haven
+                </h6>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-wrap items-stretch">
+          <div className="w-6/12 py-12 px-28 bg-[#f6f6f6] flex flex-col justify-center items-stretch text-center">
+            <div className="w-12 h-12 rounded-full  bg-[#fff] relative mx-auto block mb-10">
+              <i className="ph-light ph-chats text-6xl text-[var(--theme)] absolute bottom-1 right-1"></i>
+            </div>
+            <p className="text-[#778591] font-light text-[1.375rem] text-center mb-14">
+              I couldn’t be happier with the transport you did. It was great and
+              your car was amazing. Thank you for your great service and I will
+              continue to refer friends and family to you all.
+            </p>
+
+            <h6 className="text-center uppercase tracking-[.125rem] text-[.875rem] text-[#778591]">
+              Betty Finsen
+            </h6>
+          </div>
+          <img
+            src="https://quanticalabs.com/wp_themes4/autoride/wp-content/uploads/sites/2/2019/10/image_02-6.jpg?id=10494"
+            className="w-6/12"
+            alt=""
+          />
+        </div>
+      </section>
     </>
   );
 }
