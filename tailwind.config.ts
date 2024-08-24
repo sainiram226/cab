b7aec002b5803c5d08a28c7e357faf88ca28b7da
sainiram <sainiram.tnt@gmail.com>
2024-08-24 15:11:35 +0530

    MECabs New Design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,43 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    fontFamily: {
+      sans: ["ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+      display: ['"Jost", sans-seri'],
+      body: ['"Jost", sans-seri'],
+    },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+      "m2xl": { max: "1535.98px" },
+      // => @media (max-width: 1535px) { ... }
+
+      mxl: { max: "1279.98px" },
+      // => @media (max-width: 1279px) { ... }
+
+      mlg: { max: "1023.98px" },
+      // => @media (max-width: 1023px) { ... }
+
+      mmd: { max: "767.98px" },
+      // => @media (max-width: 767px) { ... }
+
+      msm: { max: "639.98px" },
+      // => @media (max-width: 639px) { ... }
+    },
   },
   plugins: [],
 };
